1642346f0d686b298601147a88e851f73ef23475
QuartzManbun <106609925+QuartzManbun@users.noreply.github.com>
2025-04-05 02:49:47 -0400

    Add files via upload

--- api/main.ts
+++ api/main.ts
@@ -3,8 +3,8 @@ import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabase = createClient(
-  Deno.env.get("https://dlueeyktbbdjcobckigd.supabase.co")!,
-  Deno.env.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsdWVleWt0YmJkamNvYmNraWdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4MTY0MjksImV4cCI6MjA1OTM5MjQyOX0.FSShxBY_4xaTWxGMy9pB7LPdcvCK0LTU0XaUf2NN3lA")!
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_KEY")!
 );
 
 serve(async (req) => {
